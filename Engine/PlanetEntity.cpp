429079a35b575f3d1b2256a4499f4547a8c46af2
Zayd963 <81951220+Zayd963@users.noreply.github.com>
2021-10-18 03:50:39 -0400

    Update PlanetEntity.cpp

--- Engine/PlanetEntity.cpp
+++ Engine/PlanetEntity.cpp
@@ -3,14 +3,14 @@
 PlanetEntity::PlanetEntity(std::shared_ptr<Camera> cam)
 {
 	camera = cam;
-	texture = camera->LoadTexture("../GFX/DefaultTexture.png");
+	texture = camera->LoadTexture("../GFX/Star.png");
 	position = { 640, 480 };
 	
 	velocity = { 0, 0 };
 	acceleration = { 0, 0 };
 	sourceRect = { 0, 0, 32, 32 };
 
-	destinationRect = { (int)position.x, (int)position.y, 64, 64 };
+	destinationRect = { (int)position.x, (int)position.y, 128, 128 };
 	radius = destinationRect.w / 2;
 	centredPosition.x = position.x + radius;
 	centredPosition.y = position.y + radius;
